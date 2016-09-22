c1723c7ab37486c8a19e0e7a114f8872f90a0a8a
Dawid Serafin <Dawid Serafin>
2016-09-22 18:20:19 +0200

    Test PC stuff

---
+++ Attributes/Attributes/Hero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    public class Hero : INotifyPropertyChanged
+    {
+        public Hero()
+        {
+            Strength = 12;
+            Agility = 12;
+            Vitality = 12;
+        }
+
+        private int strength;
+
+        public int Strength
+        {
+            get { return strength; }
+            set { strength = value; OnPropertyChanged("Strength"); }
+        }
+        private int agility;
+
+        public int Agility
+        {
+            get { return agility; }
+            set { agility = value; OnPropertyChanged("Agility"); }
+        }
+        private int vitality;
+
+        public int Vitality
+        {
+            get { return vitality; }
+            set { vitality = value; OnPropertyChanged("Vitality"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+}
---
+++ Attributes/Attributes/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Attributes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Hero hero;
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            hero = new Hero();
+            DataContext = hero;
+        }
+
+        private void StrD(object sender, RoutedEventArgs e)
+        {
+            hero.Strength--;
+        }
+
+        private void StrI(object sender, RoutedEventArgs e)
+        {
+            hero.Strength++;
+        }
+
+        private void AgiD(object sender, RoutedEventArgs e)
+        {
+            hero.Agility--;
+        }
+
+        private void AgiI(object sender, RoutedEventArgs e)
+        {
+            hero.Agility++;
+        }
+
+        private void VitD(object sender, RoutedEventArgs e)
+        {
+            hero.Vitality--;
+        }
+
+        private void VitI(object sender, RoutedEventArgs e)
+        {
+            hero.Vitality++;
+        }
+    }
+}
